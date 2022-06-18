124a1e7ba7d12cc96ec1046d9707072fbfc3f19d
anishasharma-web <56109423+anishasharma-web@users.noreply.github.com>
2022-06-18 22:24:18 +0530

    Create Sort_0_1_2.cpp

---
+++ Sort_0_1_2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int s=0,m=0,h=nums.size()-1;
+            while(m<=h){
+            if(nums[m]==0){
+                swap(nums[m],nums[s]);
+                s++; m++;
+            }
+            else if(nums[m]==1){
+                m++; 
+            }
+            else{
+                swap(nums[h],nums[m]);
+                h--;
+            }
+        }
+   
+   }
+};
